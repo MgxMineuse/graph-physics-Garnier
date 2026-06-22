b0683928edcf8fe9d1505dd9611b23d2f5bf40bb
MgxMineuse <margaux.dalbies@etu.minesparis.psl.eu>
2026-06-22 16:25:53 +0200

    Add SDF in nodes features

--- graphphysics/external/cylinder.py
+++ graphphysics/external/cylinder.py
@@ -1,7 +1,18 @@
 import torch
 from torch_geometric.data import Data
+from graphphysics.utils.nodetype import NodeType
 
 # device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def signed_distance_function(pos, node_type):
+    # for a cylinder
+    points_obstacle_mask = node_type == NodeType.OBSTACLE
+    points_cylinder = pos[points_obstacle_mask]
+    centre = torch.mean(points_cylinder, axis=0)
+    R = torch.linalg.norm(points_cylinder[0] - centre)
+
+    return torch.linalg.norm(pos - centre, dim=1) - R
 
 
 def build_features(graph: Data) -> Data:
@@ -9,6 +20,7 @@ def build_features(graph: Data) -> Data:
     timestep = graph.x[:, 4]
     pressure = graph.x[:, 3]
     current_velocity = graph.x[:, 1:3]
+    sdf = signed_distance_function(graph.pos, node_type)
     if "previous_data" in graph:
         previous_velocity = torch.tensor(graph.previous_data["velocity"])
         acceleration = current_velocity - previous_velocity
@@ -20,6 +32,7 @@ def build_features(graph: Data) -> Data:
                 pressure.unsqueeze(1),
                 timestep.unsqueeze(1),
                 graph.pos,
+                sdf.unsqueeze(1),
                 acceleration,
                 last_pressure,
                 node_type.unsqueeze(1),
@@ -33,6 +46,7 @@ def build_features(graph: Data) -> Data:
                 pressure.unsqueeze(1),
                 timestep.unsqueeze(1),
                 graph.pos,
+                sdf.unsqueeze(1),
                 node_type.unsqueeze(1),
             ),
             dim=1,
--- graphphysics/utils/loss.py
+++ graphphysics/utils/loss.py
@@ -134,3 +134,77 @@ class L2Loss(_Loss):
 
         beta = 1.5
         return torch.mean(errors) + beta * torch.mean(errors_obstacle)
+
+
+class L2Loss_sillage(_Loss):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @property
+    def __name__(self):
+        return "MSE"
+
+    def forward(
+        self,
+        target: torch.Tensor,
+        network_output: torch.Tensor,
+        node_type: torch.Tensor,
+        masks: list[NodeType],
+        selected_indexes: torch.Tensor = None,
+        **kwargs
+    ) -> torch.Tensor:
+        """
+        Computes L2 loss for nodes of specific types.
+
+        Args:
+            target (torch.Tensor): The target values.
+            network_output (torch.Tensor): The predicted values from the network.
+            node_type (torch.Tensor): Tensor containing the type of each node.
+            masks (list[NodeType]): List of NodeTypes to include in the loss calculation.
+            selected_indexes (torch.Tensor, optional): Indexes of nodes to exclude from the loss calculation.
+
+        Returns:
+            torch.Tensor: The mean squared error for the specified node types.
+
+        Note:
+            This method calculates the L2 loss only for nodes of the types specified in 'masks'.
+            If 'selected_indexes' is provided, those nodes are excluded from the loss calculation.
+        """
+
+        error_general = (network_output - target) ** 2
+
+        # loss sillage
+        mask_cylinder = _prepare_mask_for_loss(
+            network_output,
+            node_type,
+            [NodeType.OBSTACLE],
+            selected_indexes,
+        )
+        error_cylinder = error_general[mask_cylinder]
+
+        xmin = torch.min(kwargs["graph"].pos[:, 0][mask_cylinder])
+        ymin = torch.min(kwargs["graph"].pos[:, 1][mask_cylinder])
+        ymax = torch.max(kwargs["graph"].pos[:, 1][mask_cylinder])
+
+        mask_sillage = node_type == NodeType.NORMAL
+        conditions = [
+            kwargs["graph"].pos[:, 0] >= xmin,
+            kwargs["graph"].pos[:, 1] >= ymin,
+            kwargs["graph"].pos[:, 1] <= ymax,
+        ]
+        for c in conditions:
+            mask_sillage = torch.logical_and(mask_sillage, c)
+
+        error_sillage = error_general[mask_sillage]
+
+        # loss general
+        anti_mask = torch.logical_not(mask_sillage)
+        anti_mask = torch.logical_and(node_type == NodeType.NORMAL, anti_mask)
+
+        error_out = error_general[anti_mask]
+
+        return (
+            3 * torch.mean(error_cylinder)
+            + torch.mean(error_sillage)
+            + 0.5 * torch.mean(error_out)
+        )
